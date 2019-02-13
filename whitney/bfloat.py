b352d4a138473ec6f11ab4339254b2a1b09a39cc
Alex Carsello <ajcars@stanford.edu>
2019-02-13 11:03:43 -0800

    Make separate bfloat class

---
+++ whitney/bfloat.py
@@ -0,0 +1,35 @@
+from bit_vector import BitVector
+import struct
+import numpy
+
+def _bv_to_bfloat(a: BitVector):
+    assert (a.num_bits == 16)
+    # Turn in to equivalent float32
+    a_ext = BitVector.concat(a, BitVector(0, 16))
+    # Change to 4 raw bytes
+    raw_data = struct.pack('<I', a_ext.as_uint())
+    # Re-interpret raw-bytes as np.float32
+    fp = np.frombuffer(raw_data, dtype = np.float32)
+    return fp
+   
+def _bfloat_to_bv(fp: np.float32):
+    # Change np.float32 to 4 raw bytes
+    raw_data = struct.pack('<f', fp)
+    # Re-interpret 4 raw bytes as 2 unsigned ints
+    # Convert float32 to bfloat16 by taking the upper 
+    # 2 bytes (chop off 16 fraction bits)
+    bv_value = struct.unpack('<HH', raw_data)[1]
+    # Create BitVector from value
+    bv = BitVector(bv_value, 16)
+    return bv
+
+class BFloat16(BitVector):
+    def __add__(self, other):
+        a = _bv_to_bfloat(self)
+        b = _bv_to_bfloat(other)
+        return _bfloat_to_bv(a + b)
+    
+    def __mul__(self, other):
+        a = _bv_to_bfloat(self)
+        b = _bv_to_bfloat(other)
+        return _bfloat_to_bv(a * b)
