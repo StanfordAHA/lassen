072d6666a42cf6d78be4fe1cd068932b35062655
Alex Carsello <ajcars@stanford.edu>
2019-03-07 13:04:26 -0800

    Update bfloat to use new bv

--- lassen/bfloat.py
+++ lassen/bfloat.py
@@ -1,11 +1,11 @@
-from bit_vector import BitVector
+from hwtypes import BitVector
 import struct
 import numpy as np
 
 def _bv_to_bfloat(a: BitVector):
     assert (a.num_bits == 16)
     # Turn in to equivalent float32
-    a_ext = BitVector.concat(a, BitVector(0, 16))
+    a_ext = BitVector.concat(a, BitVector[16](0))
     # Change to 4 raw bytes
     raw_data = struct.pack('<I', a_ext.as_uint())
     # Re-interpret raw-bytes as np.float32
@@ -20,7 +20,7 @@ def _bfloat_to_bv(fp: np.float32):
     # 2 bytes (chop off 16 fraction bits)
     bv_value = struct.unpack('<HH', raw_data)[1]
     # Create BitVector from value
-    bv = BitVector(bv_value, 16)
+    bv = BitVector[16](bv_value)
     return bv
 
 class BFloat16(BitVector):
