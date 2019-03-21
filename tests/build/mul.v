c853675fba9f9b33e493998f578f0b93a6d1e2b5
Lenny Truong <lenny@stanford.edu>
2019-03-20 19:31:37 -0700

    Revert mul.v to be a stub

--- tests/build/mul.v
+++ tests/build/mul.v
@@ -4,9 +4,6 @@ module mul (
   input [15:0] in1,
   output [15:0] out
 );
-  wire [2:0] fmul_res_x;
-
-  CW_fp_mult #(.sig_width(10), .exp_width(8), .ieee_compliance(0)) mul1 (.a({in0,3'd0}),.b({in1,3'd0}),.rnd(3'd0),.z({fmul_res,fmul_res_x}),.status());
-
+assign out = 0;
 
 endmodule
