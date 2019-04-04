fd9f30a62346f750eabb8076a8e8c0113155fbfe
Ross <rdaly525@stanford.edu>
2019-04-04 12:34:38 -0700

    added family

--- lassen/mem/isa.py
+++ lassen/mem/isa.py
@@ -1,120 +1,29 @@
 from hwtypes import BitVector, Bit
 from peak.adt import Enum, Product
-from .cond import gen_cond_type
-from .mode import gen_mode_type
-from .lut import gen_lut_type
 import magma as m
 from functools import lru_cache
-
-
 
 width = 16
 depth = 1024
 
 def gen_mem_init(family,width=16,depth=1024):
-    class MemInit(family.Tuple[*[BitVector[width] for _ in range(depth)]]):
+    class MemInit(family.Tuple[*[family.BitVector[width] for _ in range(depth)]]):
         pass
     return MemInit
+
 def gen_mem_mode(family):
+    class MemMode(family.Enum):
+        Ram = 0
+        Fifo = 0
+        Linebuffer = 0
     return MemMode
-class MemMode(family.Enum):
-    Ram = 0
-    Fifo = 0
-    Linebuffer = 0
 
+def gen_mem_instr(family,width=16,depth=1024):
+    MemMode = gen_mem_mode(family)
+    MemInit = gen_mem_init(family,width,depth)
 
-class MemInst(family.Product):
-    mode : MemMode
-    init : MemInit
-
-
-@lru_cache()
-def gen_alu_type(family):
-    class ALU(family.Enum):
-        Add = 0
-        Sub = 1
-        Abs = 3
-        GTE_Max = 4
-        LTE_Min = 5
-        Sel = 8
-        Mult0 = 0xb
-        Mult1 = 0xc
-        Mult2 = 0xd
-        SHR = 0xf
-        SHL = 0x11
-        Or = 0x12
-        And = 0x13
-        XOr = 0x14
-        FP_add = 0x15
-        FP_mult = 0x16
-        FGetMant = 0x92
-        FAddIExp = 0x93
-        FSubExp = 0x94
-        FCnvExp2F = 0x95
-        FGetFInt = 0x96
-        FGetFFrac = 0x97
-    return ALU
-
-
-@lru_cache()
-def gen_signed_type(family):
-    """
-    Whether the operation is unsigned (0) or signed (1)
-    """
-    class Signed(family.Enum):
-        unsigned = 0
-        signed = 1
-    return Signed
-
-
-# Current PE has 16-bit data path
-DATAWIDTH = 16
-
-
-@lru_cache()
-def gen_inst_type(family):
-    """
-    https://github.com/StanfordAHA/CGRAGenerator/wiki/PE-Spec
-    """
-    ALU = gen_alu_type(family)
-    Signed = gen_signed_type(family)
-
-    Data = family.BitVector[DATAWIDTH]
-    # Constant values for registers
-    RegA_Const = family.BitVector[DATAWIDTH]
-    RegB_Const = family.BitVector[DATAWIDTH]
-    RegD_Const = family.Bit
-    RegE_Const = family.Bit
-    RegF_Const = family.Bit
-
-    Mode = gen_mode_type(family)
-    LUT = gen_lut_type(family)
-    Cond = gen_cond_type(family)
-
-    # Modes for registers
-    RegA_Mode = Mode
-    RegB_Mode = Mode
-    RegD_Mode = Mode
-    RegE_Mode = Mode
-    RegF_Mode = Mode
-
-    class Inst(family.Product):
-        """
-        Each configuration is given by the following fields
-        """
-        alu: ALU           # ALU operation
-        signed_: Signed    # unsigned or signed
-        lut: LUT           # LUT operation as a 3-bit LUT
-        cond: Cond         # Condition code (see cond.py)
-        rega: RegA_Mode    # RegA mode (see mode.py)
-        data0: RegA_Const  # RegA constant (16-bits)
-        regb: RegB_Mode    # RegB mode
-        data1: RegB_Const  # RegB constant (16-bits)
-        regd: RegD_Mode    # RegD mode
-        bit0: RegD_Const   # RegD constant (1-bit)
-        rege: RegE_Mode    # RegE mode
-        bit1: RegE_Const   # RegE constant (1-bit)
-        regf: RegF_Mode    # RegF mode
-        bit2: RegF_Const   # RegF constant (1-bit)
-    return Inst
-
+    class MemInstr(family.Product):
+        mode : MemMode
+        init : MemInit
+    return MemInstr
+   
