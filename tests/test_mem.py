41f36de3de949f793cdda6563401d592bd4de89c
Ross <rdaly525@stanford.edu>
2019-04-04 15:44:42 -0700

    started mem rewrite test

---
+++ tests/test_mem.py
@@ -0,0 +1,13 @@
+import lassen.mem as mem 
+from hwtypes import BitVector
+import coreir
+import metamapper as mm
+import pytest
+
+def instr_lambda(inst):
+    inst.config
+
+rom_rewrite = mm.Peak1to1(
+)
+
+def 
