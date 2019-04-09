419d79e1340e2b48830104c2b5e79048dfb98fc0
Ross <rdaly525@stanford.edu>
2019-04-09 13:40:08 -0700

    started test

--- tests/test_mem.py
+++ tests/test_mem.py
@@ -4,10 +4,31 @@ import coreir
 import metamapper as mm
 import pytest
 
-def instr_lambda(inst):
-    inst.config
+      {"clk", c->Named("coreir.clkIn")},
+      {"rdata", c->Bit()->Arr(width)},
+      {"raddr", c->BitIn()->Arr(awidth)},
+      {"ren", c->BitIn()},
 
-rom_rewrite = mm.Peak1to1(
-)
+
+def test_mem_rr():
+    c = coreir.Context()
+    mapper = mm.PeakMapper(c,"pe_ns")
+    
+    mem = mapper.add_peak_primitive("MEM",mem.gen_mem)
+    
+    rom = c.get_namespace("memory").generators['rom2'](width=16,depth=1024)
+
+    def instr_lambda(inst):
+        init_json = inst.config["init"].value
+        return asm.const(cval)
+        
+    #Adds a simple "1 to 1" rewrite rule
+    mapper.add_rewrite_rule(mm.Peak1to1(
+        rom,
+        PE,
+        instr_lambda,
+        dict(out="alu_res")
+    ))
+ 
 
 def 
