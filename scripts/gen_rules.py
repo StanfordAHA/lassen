6581096763b187d6336c8db6e58d13d99624e4c4
Ross Daly <rdaly525@stanford.edu>
2019-05-06 18:14:20 -0700

    generated most of the coreir rules

--- scripts/gen_rules.py
+++ scripts/gen_rules.py
@@ -18,13 +18,14 @@ def discover(file_name):
             inst.regb == type(inst.regb).BYPASS and
             inst.regd == type(inst.regd).BYPASS and
             inst.rege == type(inst.rege).BYPASS and
-            inst.regf == type(inst.regf).BYPASS
+            inst.regf == type(inst.regf).BYPASS and
+            inst.alu.name[0] != 'F'
         )
     mapper.add_discover_constraint(bypass_mode)
-    rrs = mapper.discover_peak_rewrite_rules(width=16,coreir_primitives=["add","sub","or"],serialize=True,verbose=True)
+    rrs = mapper.discover_peak_rewrite_rules(width=16,serialize=True,verbose=True)
     print(rrs)
 
     with open(file_name,'w') as jfile:
         json.dump(rrs,jfile,indent=2)
 
-discover('rules/simple.json')
+#discover('rules/_all.json')
