4248ede5b2ddf57c9db0a0e4b3e9ef117ed96fd3
Ross <rdaly525@stanford.edu>
2019-05-13 14:22:46 -0700

    added stdlib for lassen. Added simple fma test and complex function

---
+++ lassen/stdlib/fma.py
@@ -0,0 +1,20 @@
+from hwtypes import BitVector
+from lassen import gen_pe, asm, mem
+from peak import Peak
+
+#This is a complex operation that implements a Fused multiply add
+def gen_FMA(family):
+    Data = family.BitVector[16]
+    PE = gen_pe(family)
+    class FMA(Peak):
+        def __init__(self):
+            self.pe1 = PE()
+            self.pe2 = PE()
+
+        def __call__(self,in0 : Data, in1 : Data, in2 : Data):
+            inst1 = asm.smult0()
+            inst2 = asm.add()
+            pe1_out,_,_ = self.pe1(inst1,in0,in1)
+            pe2_out,_,_ = self.pe2(inst2,pe1_out,in2)
+            return pe2_out
+    return FMA
