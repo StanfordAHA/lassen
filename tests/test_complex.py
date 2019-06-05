89af36d3729fde3e8325f4d0fb03f6c51ae59ee7
nikhilbhagdikar <nikhil3@kiwi.Stanford.EDU>
2019-06-05 01:08:00 -0700

    Add error bounds for div/log/exp. Add classes for complex fp ops.

--- tests/test_complex.py
+++ tests/test_complex.py
@@ -1,12 +1,13 @@
 from lassen.stdlib.fma import gen_FMA
+from lassen.stdlib.fpops import gen_fdiv, gen_fln, gen_fexp
 from lassen.isa import DATAWIDTH
 from hwtypes import BitVector, Bit, SIntVector
 from lassen.sim import gen_pe
+from lassen.utils import float2bfbin, bfbin2float
 import pytest
 import lassen.asm as asm
 import math
 import random
-
 
 Bit = Bit
 Data = BitVector[DATAWIDTH]
@@ -15,157 +16,138 @@ SData = SIntVector[DATAWIDTH]
 NTESTS=16
 
 FMA = gen_FMA(BitVector.get_family())
+DIV = gen_fdiv(BitVector.get_family()) 
+LN  = gen_fln(BitVector.get_family())
+EXP = gen_fexp(BitVector.get_family())
 
 @pytest.mark.parametrize("args", [
     (random.randint(-10,10),
      random.randint(-10,10),
      random.randint(-10,10))
     for _ in range(NTESTS) ] )
+
 def test_fma(args):
     fma = FMA()
     assert SData(args[0]*args[1]+args[2]) == fma(SData(args[0]), SData(args[1]), SData(args[2]))
 
+def get_random_float(power_range = 50):
+    sign = -1 if (random.random() < 0.5) else 1
+    mant = float(random.random()+1.0)
+    power = (-1 * power_range) + int(random.random()*(power_range * 2))
+    res = sign * mant * float((2**power))
+    return res
+
 def test_div():
     test_vectors = []
-    for vector_count in range(50):
+    eps = (2**-8)
+    #Corner case test vectors
+    #Random test vectors
+    for vector_count in range(200):
         divident_sp = get_random_float()
         divisor_sp = get_random_float()
         divident_bfloat_str = float2bfbin(divident_sp)
         divisor_bfloat_str = float2bfbin(divisor_sp)
         divident_bfp = bfbin2float(divident_bfloat_str)
         divisor_bfp = bfbin2float(divisor_bfloat_str)
-        quotient_bfp = bfbin2float(float2bfbin(divident_bfp / divisor_bfp))
-        quotient_bfloat_str = float2bfbin(quotient_bfp)
-        min_accuracy = 2
+        quotient = bfbin2float(float2bfbin(divident_bfp / divisor_bfp))
+        quotient_bfloat_str = float2bfbin(quotient)
+
+        divisor_exp = int(divisor_bfloat_str[1:9],2)-127
+        max_error =  2*math.fabs(eps*(divident_bfp/divisor_bfp)) 
         test_vectors.append(
-            [divident_bfloat_str, divisor_bfloat_str, quotient_bfloat_str, min_accuracy])
+            [divident_bfloat_str, divisor_bfloat_str, quotient_bfloat_str, max_error])
 
-    # START_TEST result = op_a / op_b
-    pe_get_mant = gen_pe(BitVector.get_family())()
-    mem_lut = tlut()
-    pe_scale_res = gen_pe(BitVector.get_family())()
-    pe_mult = gen_pe(BitVector.get_family())()
-
-    inst1 = asm.fgetmant()
-    inst2 = asm.fsubexp()
-    inst3 = asm.fp_mult()
-
+    #result = op_a / op_b
     for test_vector in test_vectors:
         op_a = Data(int(test_vector[0], 2))
         op_b = Data(int(test_vector[1], 2))
         exp_res = int(test_vector[2], 2)
-        acc = test_vector[3]
-
-        mant, d1, d2 = pe_get_mant(inst1, op_b, Data(0))
-        lookup_result = mem_lut.div_lut(mant)
-        scaled_result, d3, d4 = pe_scale_res(inst2, lookup_result, op_b)
-        result, d5, d6 = pe_mult(inst3, scaled_result, op_a)
-
-        print("div", bfbin2float(test_vector[0]), bfbin2float(test_vector[1]), bfbin2float(
-            test_vector[2]), bfbin2float("{:016b}".format(int(result))))
-        assert abs(exp_res-int(result)) <= acc
+        max_error = test_vector[3]
+        div = DIV()
+        result = div(op_a, op_b, 0) 
+        golden_res = bfbin2float(test_vector[2])
+        actual_res = bfbin2float("{:016b}".format(int(result)))
+        delta = math.fabs(golden_res - actual_res)
+        print("div", bfbin2float(test_vector[0]), bfbin2float(test_vector[1]),
+              golden_res,actual_res,delta,max_error)
+        assert delta <= max_error
 
 def test_ln():
     test_vectors = []
-    for vector_count in range(50):
+    eps = (2**-8)
+    #Corner case test vectors
+    test_vectors.append([float2bfbin(2.0), float2bfbin(0), float2bfbin(math.log(2)), 2*eps])
+    #Random test vectors
+    for vector_count in range(200):
         num = math.fabs(get_random_float())
         num_bfloat_str = float2bfbin(num)
         num_bfp = bfbin2float(num_bfloat_str)
-        res_bfp = bfbin2float(float2bfbin(math.log(num_bfp)))
-        res_bfloat_str = float2bfbin(res_bfp)
-        min_accuracy = 3
-        test_vectors.append(
-            [num_bfloat_str, float2bfbin(0), res_bfloat_str, min_accuracy])
+        res = bfbin2float(float2bfbin(math.log(num_bfp)))
+        res_bfloat_str = float2bfbin(res)
 
-    test_vectors.append([float2bfbin(2.0), float2bfbin(
-        0), float2bfbin(math.log(2)), min_accuracy])
-    # START_TEST result = ln(op_a)
-    pe_get_mant = gen_pe(BitVector.get_family())()
-    pe_get_exp = gen_pe(BitVector.get_family())()
-    mem_lut = tlut()
-    pe_mult = gen_pe(BitVector.get_family())()
-    pe_add = gen_pe(BitVector.get_family())()
+        num_exp = int(num_bfloat_str[1:9],2)-127
+        res_exp = int(res_bfloat_str[1:9],2)-127
+        max_error =  2*(eps + math.fabs(eps*num_exp) + eps)
+        test_vectors.append([num_bfloat_str, float2bfbin(0), res_bfloat_str,max_error])
 
-    inst1 = asm.fgetmant()
-    inst2 = asm.fcnvexp2f()
-    inst3 = asm.fp_mult()
-    inst4 = asm.fp_add()
-
-    ln2 = math.log(2)
-    ln2_bf = int(float2bfbin(ln2), 2)
-
+    #result = ln(op_a)
     for test_vector in test_vectors:
         op_a = Data(int(test_vector[0], 2))
         op_b = Data(int(test_vector[1], 2))
         exp_res = int(test_vector[2], 2)
-        acc = test_vector[3]
+        max_error = test_vector[3]
 
-        const1 = Data(ln2_bf)
+        ln = LN()
+        result = ln(op_a,0,0)
+        golden_res = bfbin2float(test_vector[2])
+        actual_res = bfbin2float("{:016b}".format(int(result)))
+        delta = math.fabs(golden_res - actual_res)
+        print("ln", bfbin2float(test_vector[0]), bfbin2float(test_vector[1]),
+              golden_res,actual_res,delta, max_error)
 
-        mant, d1, d2 = pe_get_mant(inst1, op_a, op_b)
-        fexp, d1, d2 = pe_get_exp(inst2, op_a, op_b)
-        lut_res = mem_lut.ln_lut(mant)
-        mult, d1, d2 = pe_mult(inst3, fexp, const1)
-        result, d1, d2 = pe_mult(inst4, Data(lut_res), mult)
-        print("ln", bfbin2float(test_vector[0]), bfbin2float(test_vector[1]), bfbin2float(
-            test_vector[2]), bfbin2float("{:016b}".format(int(result))))
-        assert abs(exp_res-int(result)) <= acc
+        assert delta <= max_error
 
 def test_exp():
     test_vectors = []
+    eps = (2**-8)
+    #Corner case test vectors
+    test_vectors.append([float2bfbin(2.0), float2bfbin(
+        0), float2bfbin(math.exp(2)), 3])
+    test_vectors.append([float2bfbin(-2.0), float2bfbin(0),
+                         float2bfbin(math.exp(-2)), 3])
+    test_vectors.append([float2bfbin(1.0), float2bfbin(
+        0), float2bfbin(math.exp(1.0)), 3])
+    test_vectors.append([float2bfbin(0.0), float2bfbin(
+        0), float2bfbin(math.exp(0)), 3])
+    test_vectors.append([float2bfbin(-10.0), float2bfbin(0),
+                         float2bfbin(math.exp(-10)), 3])
+    test_vectors.append([float2bfbin(5.0), float2bfbin(
+        0), float2bfbin(math.exp(5)), 3])
+
+    #Random test vectors    
     for vector_count in range(50):
-        num = get_random_float(2)
+        num = get_random_float(4)
         num_bfloat_str = float2bfbin(num)
         num_bfp = bfbin2float(num_bfloat_str)
         res_bfp = bfbin2float(float2bfbin(math.exp(num_bfp)))
         res_bfloat_str = float2bfbin(res_bfp)
-        min_accuracy = 3
+        max_error = 2.0*res_bfp*(math.fabs(2**(2*num*eps)-1) + eps)
         test_vectors.append(
-            [num_bfloat_str, float2bfbin(0), res_bfloat_str, min_accuracy])
+            [num_bfloat_str, float2bfbin(0), res_bfloat_str, max_error])
 
-    test_vectors.append([float2bfbin(2.0), float2bfbin(
-        0), float2bfbin(math.exp(2)), min_accuracy])
-    test_vectors.append([float2bfbin(-2.0), float2bfbin(0),
-                         float2bfbin(math.exp(-2)), min_accuracy])
-    test_vectors.append([float2bfbin(1.0), float2bfbin(
-        0), float2bfbin(math.exp(1.0)), min_accuracy])
-    test_vectors.append([float2bfbin(0.0), float2bfbin(
-        0), float2bfbin(math.exp(0)), min_accuracy])
-    test_vectors.append([float2bfbin(-10.0), float2bfbin(0),
-                         float2bfbin(math.exp(-10)), min_accuracy])
-    test_vectors.append([float2bfbin(5.0), float2bfbin(
-        0), float2bfbin(math.exp(5)), min_accuracy])
-    # START_TEST result = exp(op_a)
-    pe_get_int = gen_pe(BitVector.get_family())()
-    pe_get_frac = gen_pe(BitVector.get_family())()
-    mem_lut = tlut()
-    pe_incr_exp = gen_pe(BitVector.get_family())()
-
-    pe_div_mult = gen_pe(BitVector.get_family())()
-
-    ln2_inv = 1.4453125  # 1.0/math.log(2)
-    ln2_inv_bf = int(float2bfbin(ln2_inv), 2)
-    const1 = Data(ln2_inv_bf)
-
+    #result = exp(op_a)
     for test_vector in test_vectors:
         op_a = Data(int(test_vector[0], 2))
         op_b = Data(int(test_vector[1], 2))
         exp_res = int(test_vector[2], 2)
-        acc = test_vector[3]
+        max_error = test_vector[3]
 
-        # Perform op_a/ln(2)
-        inst1 = asm.fp_mult()
-        div_res, d5, d6 = pe_div_mult(inst1, const1, op_a)
+        exp = EXP()
+        result = exp(op_a, 0, 0) 
+        golden_res = bfbin2float(test_vector[2])
+        actual_res = bfbin2float("{:016b}".format(int(result)))
+        delta = math.fabs(golden_res - actual_res)
+        print("ln", bfbin2float(test_vector[0]), bfbin2float(test_vector[1]),
+              golden_res,actual_res,delta, max_error)
 
-        # Compute 2**op_a
-        inst2 = asm.fgetfint()
-        inst3 = asm.fgetffrac()
-        inst4 = asm.faddiexp()
-
-        fint, d1, d2 = pe_get_int(inst2, div_res, op_b)
-        ffrac, d1, d2 = pe_get_frac(inst3, div_res, op_b)
-        lut_res = mem_lut.exp_lut(ffrac)
-        result, d1, d2 = pe_incr_exp(inst4, lut_res, fint)
-        print("exp", bfbin2float(test_vector[0]), bfbin2float(test_vector[1]), bfbin2float(
-            test_vector[2]), bfbin2float("{:016b}".format(int(result))))
-        assert abs(exp_res-int(result)) <= acc
+        assert delta <= max_error
