f30c5a93357d5817643d507a2be168a7c28f1c40
Caleb Donovick <donovick@cs.stanford.edu>
2020-01-10 17:50:06 -0800

    Use peak to get rtl_res

--- tests/test_pe.py
+++ tests/test_pe.py
@@ -107,7 +107,7 @@ def rtl_tester(test_op, data0=None, data1=None, bit0=None, bit1=None, bit2=None,
 
     if res is not None:
         if is_fp_op:
-            rtl_res = tester.circuit.O0
+            rtl_res = tester.peak(tester._circuit.O0)
             # select lsb from rtl_res and the other bits from res
             # https://graphics.stanford.edu/~seander/bithacks.html#MaskedMerge
             masked_res = res ^ ((res ^ rtl_res) & 1)
