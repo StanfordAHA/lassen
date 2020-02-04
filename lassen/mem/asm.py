016930ede25e2c56ebf06ea3f7be366a8283c2e3
Ross <rdaly525@stanford.edu>
2020-02-04 14:14:47 -0800

    Updated memory to use appropriate family_closure

--- lassen/mem/asm.py
+++ lassen/mem/asm.py
@@ -3,9 +3,10 @@ from .sim import width, depth
 from hwtypes import BitVector
 
 Data = BitVector[width]
-MemInstr, (Rom, Fifo, LineBuffer) = gen_mem_instr(BitVector.get_family(),width,depth)
+MemInstr = gen_mem_instr(width,depth)
+Rom = MemInstr.field_dict['rom']
 
 def rom(init):
     assert len(init)==depth
-    instr = MemInstr(Rom(init=Rom.init(*(Data(val) for val in init))))
+    instr = MemInstr(rom=Rom(init=Rom.init(*(Data(val) for val in init))))
     return instr
