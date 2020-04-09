aa8836eb7018275fd9e3bc6c4cf938a0e6816abd
Ross <rdaly525@stanford.edu>
2020-04-09 10:47:00 -0700

    added new test that compiles lassen exactly like garnet does

--- tests/test_rtl.py
+++ tests/test_rtl.py
@@ -5,6 +5,9 @@ from lassen.lut import LUT_fc
 from lassen.sim import PE_fc
 from hwtypes import BitVector
 import magma
+from collections import OrderedDict
+from peak.assembler import Assembler
+import peak
 
 def test_cond():
     Cond_magma = Cond_fc(magma.get_family())
@@ -20,4 +23,28 @@ def test_PE():
 
 def test_LUT():
     LUT_magma = LUT_fc(magma.get_family())
+
+
+def test_wrapped_PE():
+    class HashableDict(dict):
+        def __hash__(self):
+            return hash(tuple(sorted(self.keys())))
+
+
+    pe = PE_fc(BitVector.get_family())
+    #Lassen's name for the ISA is 'inst', so this is hardcoded
+    __instr_name = 'inst'
+    __instr_type = pe.input_t.field_dict['inst']
+    __inputs = OrderedDict(pe.input_t.field_dict)
+    __inputs['inst']
+    __outputs = OrderedDict(pe.output_t.field_dict)
+    circuit = PE_fc(magma.get_family())
+    __asm = Assembler(__instr_type)
+    instr_magma_type = type(circuit.interface.ports[__instr_name])
+    __circuit = peak.wrap_with_disassembler(
+        circuit, __asm.disassemble, __asm.width,
+        HashableDict(__asm.layout),
+        instr_magma_type)
+    assert __circuit is not None
+
 
