70cfb4f389665ce4198b32242dede82f2cf96998
Ross <rdaly525@stanford.edu>
2020-05-05 12:45:27 -0700

    changed mapper test to look for a multiply

--- tests/test_mapper.py
+++ tests/test_mapper.py
@@ -4,18 +4,19 @@ from peak.mapper import ArchMapper
 from peak.mapper.utils import pretty_print_binding
 
 @family_closure
-def Add_fc(family):
+def Mul_fc(family):
     Data = family.BitVector[16]
     @family.assemble(locals(), globals())
-    class Add(Peak):
+    class Mul(Peak):
         def __call__(self, a: Data, b:Data) -> Data:
-            return a + b
-    return Add
+            return a * b
+    return Mul
 
-def test_add():
+def test_mul():
     arch_fc = ALU_fc
-    ir_fc = Add_fc
+    ir_fc = Mul_fc
     arch_mapper = ArchMapper(arch_fc)
     ir_mapper = arch_mapper.process_ir_instruction(ir_fc)
+    assert ir_mapper.formula is not None
     rr = ir_mapper.solve('z3')
     assert rr is not None
