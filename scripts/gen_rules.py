51ea5a4ba68c11fde879447db4db2a8a2910488f
Jackson Melchert <melchert@stanford.edu>
2021-02-09 10:10:07 -0800

    Removed external loop in rewrite rule solving

--- scripts/gen_rules.py
+++ scripts/gen_rules.py
@@ -51,7 +51,7 @@ for op in lassen_ops:
     print(f"Searching for {op}", flush=True)
     ir_fc = CoreIRNodes.peak_nodes[op]
     ir_mapper = arch_mapper.process_ir_instruction(ir_fc)
-    rewrite_rule = ir_mapper.solve('z3',external_loop=True)
+    rewrite_rule = ir_mapper.solve()
     assert rewrite_rule is not None
     serialized_rr = rewrite_rule.serialize_bindings()
     all_rrs[op] = serialized_rr
