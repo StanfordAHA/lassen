c39cef2fe85c524dac562f87efc08b2c641e8b48
Jackson Melchert <melchert@stanford.edu>
2021-11-03 11:54:26 -0700

    Added all pipelined rules

---
+++ lassen/rewrite_rules/mul_pipelined.py
@@ -0,0 +1,20 @@
+
+from peak import Peak, family_closure, Const
+from peak import family
+from peak.family import AbstractFamily
+
+@family_closure
+def mul_fc(family: AbstractFamily):
+    Data = family.BitVector[16]
+    Data32 = family.Unsigned[32]
+    SInt = family.Signed[16]
+    UInt = family.Unsigned[16]
+    Bit = family.Bit
+    @family.assemble(locals(), globals())
+    class mul(Peak):
+        def __call__(self, in1 : Data, in0 : Data) -> Data:
+            
+            return Data(UInt(in0) * UInt(in1))
+    
+    return mul
+    
