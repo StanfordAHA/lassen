90d979b5716ab884d219fa6f5f9e274e061e0966
Jackson Melchert <melchert@stanford.edu>
2021-11-14 21:10:22 -0800

    Added const pipelined rules

--- lassen/rewrite_rules/const_abs_pipelined.py
+++ lassen/rewrite_rules/const_abs_pipelined.py
@@ -4,17 +4,17 @@ from peak import family
 from peak.family import AbstractFamily
 
 @family_closure
-def bit_const_pipelined_fc(family: AbstractFamily):
+def const_abs_pipelined_fc(family: AbstractFamily):
     Data = family.BitVector[16]
     Data32 = family.Unsigned[32]
     SInt = family.Signed[16]
     UInt = family.Unsigned[16]
     Bit = family.Bit
     @family.assemble(locals(), globals())
-    class bit_const_pipelined(Peak):
-        def __call__(self, in0 : Const(Bit)) -> Bit:
+    class const_abs_pipelined(Peak):
+        def __call__(self, in0 : Const(Data)) -> Data:
             
-            return in0
+            return Data((SInt(in0) >= SInt(0)).ite(SInt(in0), SInt(in0)*SInt(-1)))
     
-    return bit_const_pipelined
+    return const_abs_pipelined
     
--- lassen/rewrite_rules/const_bit_and_pipelined.py
+++ lassen/rewrite_rules/const_bit_and_pipelined.py
@@ -4,17 +4,17 @@ from peak import family
 from peak.family import AbstractFamily
 
 @family_closure
-def bit_const_pipelined_fc(family: AbstractFamily):
+def const_bit_and_pipelined_fc(family: AbstractFamily):
     Data = family.BitVector[16]
     Data32 = family.Unsigned[32]
     SInt = family.Signed[16]
     UInt = family.Unsigned[16]
     Bit = family.Bit
     @family.assemble(locals(), globals())
-    class bit_const_pipelined(Peak):
-        def __call__(self, in0 : Const(Bit)) -> Bit:
+    class const_bit_and_pipelined(Peak):
+        def __call__(self, in1 : Bit, in0 : Const(Bit)) -> Bit:
             
-            return in0
+            return Bit(Bit(in0) & Bit(in1))
     
-    return bit_const_pipelined
+    return const_bit_and_pipelined
     
