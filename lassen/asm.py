b7b6f0639c5f31d641a6d1931343aea41cf124b0
Jackson Melchert <melchert@stanford.edu>
2022-04-04 15:31:50 -0700

    Added rewrite rules

--- lassen/asm.py
+++ lassen/asm.py
@@ -7,10 +7,8 @@ Inst = Inst_fc(PyFamily())
 LUT_t = Inst.lut
 Cond_t = Inst.cond
 Mode_t = Inst.rega
-Op_t = Inst.op
-ALU_t = Op_t.alu
-FPU_t = Op_t.fpu
-FPCustom_t = Op_t.fp_custom
+ALU_t = Inst.alu
+
 Signed_t = Inst.signed
 DataConst = Inst.data0
 BitConst = Inst.bit0
@@ -39,168 +37,115 @@ def inst(alu, signed=Signed_t.unsigned, lut=0, cond=Cond_t.Z,
 # helper functions to format configurations
 
 def add(**kwargs):
-    return inst(Op_t(alu=ALU_t.Add), **kwargs)
+    return inst(ALU_t.Add, **kwargs)
 
 def sub(**kwargs):
-    return inst(Op_t(alu=ALU_t.Sub), **kwargs)
+    return inst(ALU_t.Sub, **kwargs)
 
 def adc(**kwargs):
-    return inst(Op_t(alu=ALU_t.Adc), **kwargs)
+    return inst(ALU_t.Adc, **kwargs)
 
 def sbc(**kwargs):
-    return inst(Op_t(alu=ALU_t.Sbc), **kwargs)
+    return inst(ALU_t.Sbc, **kwargs)
 
 def neg(**kwargs):
-    return inst(Op_t(alu=ALU_t.Sub), **kwargs)
+    return inst(ALU_t.Sub, **kwargs)
 
 def umult0(**kwargs):
-    return inst(Op_t(alu=ALU_t.Mult0), **kwargs)
+    return inst(ALU_t.Mult0, **kwargs)
 
 def umult1(**kwargs):
-    return inst(Op_t(alu=ALU_t.Mult1), **kwargs)
+    return inst(ALU_t.Mult1, **kwargs)
 
 def umult2(**kwargs):
-    return inst(Op_t(alu=ALU_t.Mult2), **kwargs)
+    return inst(ALU_t.Mult2, **kwargs)
 
 def smult0(**kwargs):
-    return inst(Op_t(alu=ALU_t.Mult0), signed=Signed_t.signed, **kwargs)
+    return inst(ALU_t.Mult0, signed=Signed_t.signed, **kwargs)
 
 def smult1(**kwargs):
-    return inst(Op_t(alu=ALU_t.Mult1), signed=Signed_t.signed, **kwargs)
+    return inst(ALU_t.Mult1, signed=Signed_t.signed, **kwargs)
 
 def smult2(**kwargs):
-    return inst(Op_t(alu=ALU_t.Mult2), signed=Signed_t.signed, **kwargs)
-
-def fgetmant(**kwargs):
-    return inst(Op_t(fp_custom=FPCustom_t.FGetMant), **kwargs)
-
-def fp_add(**kwargs):
-    return inst(Op_t(fpu=FPU_t.FP_add), **kwargs)
-
-def fp_sub(**kwargs):
-    return inst(Op_t(fpu=FPU_t.FP_sub), **kwargs)
-
-def fp_mul(**kwargs):
-    return inst(Op_t(fpu=FPU_t.FP_mul), **kwargs)
-
-def fp_cmp(cond, **kwargs):
-    return inst(Op_t(fpu=FPU_t.FP_cmp), cond=cond, **kwargs)
-
-def fp_gt(**kwargs):
-    return fp_cmp(Cond_t.FP_GT, **kwargs)
-
-def fp_ge(**kwargs):
-    return fp_cmp(Cond_t.FP_GE, **kwargs)
-
-def fp_lt(**kwargs):
-    return fp_cmp(Cond_t.FP_LT, **kwargs)
-
-def fp_le(**kwargs):
-    return fp_cmp(Cond_t.FP_LE, **kwargs)
-
-def fp_eq(**kwargs):
-    return fp_cmp(Cond_t.FP_EQ, **kwargs)
-
-def fp_neq(**kwargs):
-    return fp_cmp(Cond_t.FP_NE, **kwargs)
+    return inst(ALU_t.Mult2, signed=Signed_t.signed, **kwargs)
 
 
-def faddiexp(**kwargs):
-    return inst(Op_t(fp_custom=FPCustom_t.FAddIExp), **kwargs)
-
-def fsubexp(**kwargs):
-    return inst(Op_t(fp_custom=FPCustom_t.FSubExp), **kwargs)
-
-def fcnvexp2f(**kwargs):
-    return inst(Op_t(fp_custom=FPCustom_t.FCnvExp2F), **kwargs)
-
-def fgetfint(**kwargs):
-    return inst(Op_t(fp_custom=FPCustom_t.FGetFInt), **kwargs)
-
-def fgetffrac(**kwargs):
-    return inst(Op_t(fp_custom=FPCustom_t.FGetFFrac), **kwargs)
-
-def fcnvsint2f(**kwargs):
-    return inst(Op_t(fp_custom=FPCustom_t.FCnvInt2F), signed=Signed_t.signed, **kwargs)
-
-def fcnvuint2f(**kwargs):
-    return inst(Op_t(fp_custom=FPCustom_t.FCnvInt2F), signed=Signed_t.unsigned, **kwargs)
 
 
 def and_(**kwargs):
-    return inst(Op_t(alu=ALU_t.And), **kwargs)
+    return inst(ALU_t.And, **kwargs)
 
 def or_(**kwargs):
-    return inst(Op_t(alu=ALU_t.Or), **kwargs)
+    return inst(ALU_t.Or, **kwargs)
 
 def xor(**kwargs):
-    return inst(Op_t(alu=ALU_t.XOr), **kwargs)
+    return inst(ALU_t.XOr, **kwargs)
 
 def lsl(**kwargs):
-    return inst(Op_t(alu=ALU_t.SHL), **kwargs)
+    return inst(ALU_t.SHL, **kwargs)
 
 def lsr(**kwargs):
-    return inst(Op_t(alu=ALU_t.SHR), **kwargs)
+    return inst(ALU_t.SHR, **kwargs)
 
 def asr(**kwargs):
-    return inst(Op_t(alu=ALU_t.SHR), signed=Signed_t.signed, **kwargs)
+    return inst(ALU_t.SHR, signed=Signed_t.signed, **kwargs)
 
 def sel(**kwargs):
-    return inst(Op_t(alu=ALU_t.Sel), **kwargs)
+    return inst(ALU_t.Sel, **kwargs)
 
 def abs(**kwargs):
-    return inst(Op_t(alu=ALU_t.Abs), signed=Signed_t.signed, **kwargs)
+    return inst(ALU_t.Abs, signed=Signed_t.signed, **kwargs)
 
 def umin(**kwargs):
-    return inst(Op_t(alu=ALU_t.LTE_Min), cond=Cond_t.ALU, **kwargs)
+    return inst(ALU_t.LTE_Min, cond=Cond_t.ALU, **kwargs)
 
 def umax(**kwargs):
-    return inst(Op_t(alu=ALU_t.GTE_Max), cond=Cond_t.ALU, **kwargs)
+    return inst(ALU_t.GTE_Max, cond=Cond_t.ALU, **kwargs)
 
 def smin(**kwargs):
-    return inst(Op_t(alu=ALU_t.LTE_Min), signed=Signed_t.signed, cond=Cond_t.ALU, **kwargs)
+    return inst(ALU_t.LTE_Min, signed=Signed_t.signed, cond=Cond_t.ALU, **kwargs)
 
 def smax(**kwargs):
-    return inst(Op_t(alu=ALU_t.GTE_Max), signed=Signed_t.signed, cond=Cond_t.ALU, **kwargs)
+    return inst(ALU_t.GTE_Max, signed=Signed_t.signed, cond=Cond_t.ALU, **kwargs)
 
 def eq(**kwargs):
-    return inst(Op_t(alu=ALU_t.Sub), cond=Cond_t.Z, **kwargs)
+    return inst(ALU_t.Sub, cond=Cond_t.Z, **kwargs)
 
 def ne(**kwargs):
-    return inst(Op_t(alu=ALU_t.Sub), cond=Cond_t.Z_n, **kwargs)
+    return inst(ALU_t.Sub, cond=Cond_t.Z_n, **kwargs)
 
 def ult(**kwargs):
-    return inst(Op_t(alu=ALU_t.Sub), cond=Cond_t.ULT, **kwargs)
+    return inst(ALU_t.Sub, cond=Cond_t.ULT, **kwargs)
 
 def ule(**kwargs):
-    return inst(Op_t(alu=ALU_t.Sub), cond=Cond_t.ULE, **kwargs)
+    return inst(ALU_t.Sub, cond=Cond_t.ULE, **kwargs)
 
 def ugt(**kwargs):
-    return inst(Op_t(alu=ALU_t.Sub), cond=Cond_t.UGT, **kwargs)
+    return inst(ALU_t.Sub, cond=Cond_t.UGT, **kwargs)
 
 def uge(**kwargs):
-    return inst(Op_t(alu=ALU_t.Sub), cond=Cond_t.UGE, **kwargs)
+    return inst(ALU_t.Sub, cond=Cond_t.UGE, **kwargs)
 
 def slt(**kwargs):
-    return inst(Op_t(alu=ALU_t.Sub), cond=Cond_t.SLT, **kwargs)
+    return inst(ALU_t.Sub, cond=Cond_t.SLT, **kwargs)
 
 def sle(**kwargs):
-    return inst(Op_t(alu=ALU_t.Sub), cond=Cond_t.SLE, **kwargs)
+    return inst(ALU_t.Sub, cond=Cond_t.SLE, **kwargs)
 
 def sgt(**kwargs):
-    return inst(Op_t(alu=ALU_t.Sub), cond=Cond_t.SGT, **kwargs)
+    return inst(ALU_t.Sub, cond=Cond_t.SGT, **kwargs)
 
 def sge(**kwargs):
-    return inst(Op_t(alu=ALU_t.Sub), cond=Cond_t.SGE, **kwargs)
+    return inst(ALU_t.Sub, cond=Cond_t.SGE, **kwargs)
 
 # implements a constant using a register and add by zero
 def const(val):
-    return inst(Op_t(alu=ALU_t.Add),
+    return inst(ALU_t.Add,
                 ra_mode=Mode_t.CONST, ra_const=val,
                 rb_mode=Mode_t.CONST, rb_const=0)
 
 def lut(val):
-    return inst(Op_t(alu=ALU_t.Add), lut=val, cond=Cond_t.LUT)
+    return inst(ALU_t.Add, lut=val, cond=Cond_t.LUT)
 
 #Using bit1 and bit2 since bit0 can be used in the ALU_t
 def lut_and():
