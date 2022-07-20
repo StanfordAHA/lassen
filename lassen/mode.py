fa30b158632f7148c01c2ed877423e291b0279c4
root <melchert@stanford.edu>
2022-07-20 15:20:26 -0700

    Updating to aha flow master

--- lassen/mode.py
+++ lassen/mode.py
@@ -33,8 +33,10 @@ def gen_register_mode(width, init):
 
             #Outputs <based on mode>, register_value
             def __call__(self, mode: Mode_t, const_: T, value: T,
-                    clk_en: Bit) -> (T):
-                if mode == Mode_t.DELAY:
+                    clk_en: Bit, config_we : Bit, config_data : T) -> (T, T):
+                if config_we:
+                    data, en = config_data, Bit(1)
+                elif mode == Mode_t.DELAY:
                     data, en = value, clk_en
                 else:
                     data, en = value, Bit(0)
@@ -42,11 +44,11 @@ def gen_register_mode(width, init):
                 reg_val = self.register(data, en)
 
                 if mode == Mode_t.CONST:
-                    return const_
+                    return const_, reg_val
                 elif mode == Mode_t.BYPASS:
-                    return value
+                    return value, reg_val
                 else: # mode == Mode_t.DELAY:
-                    return reg_val
+                    return reg_val, reg_val
         return RegisterMode
     return RegisterMode_fc
 
