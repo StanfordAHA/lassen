df89ca6d92f5c5aeaa1e7d3a41033128902fecc4
jack-melchert <56329001+jack-melchert@users.noreply.github.com>
2023-11-26 10:42:28 -0800

    Added unit tests for fp max and relu

--- tests/test_pe.py
+++ tests/test_pe.py
@@ -308,6 +308,8 @@ fp_inf_vec = [BV("inf"), BV("-inf")]
         op(asm.fp_add(), lambda x, y: x + y),
         op(asm.fp_sub(), lambda x, y: x - y),
         op(asm.fp_mul(), lambda x, y: x * y),
+        op(asm.fp_max(), lambda x, y: max(x, y)),
+        op(asm.fp_relu(), lambda x, y: max(0, x)),
     ],
 )
 @pytest.mark.parametrize(
